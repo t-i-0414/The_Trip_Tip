3a5862bf60dba89f83bdab957f84a7522b4f46ee
Takuya Iwashiro <gutlo.ol0414@gmail.com>
2019-11-28 07:36:32 +0000

    herokuへデプロイ後、herokuアプリケーション名をthe-tirp-tipに変更し、herokuドメインをアクセス許可するようconfig/environments/production.rbを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  
+  #herokuのドメインをアクセス許可
+  config.hosts << 'the-trip-tip.herokuapp.com'
 end
