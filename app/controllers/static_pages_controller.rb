0687628101443b1a3c73b1c4d32e715e03f62b67
Takuya Iwashiro <gutlo.ol0414@gmail.com>
2019-11-29 03:01:12 +0000

    静的ページについてのコントローラーを作成

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,31 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def signup
+  end
+
+  def login
+  end
+
+  def unlock_user
+  end
+
+  def rest_password
+  end
+
+  def about
+  end
+
+  def terms_of_use
+  end
+
+  def privacy
+  end
+
+  def error_404
+  end
+
+  def error_500
+  end
+end
