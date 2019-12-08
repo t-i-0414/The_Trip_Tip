4ce22a5fbe577fd543471cd60f8d1e320b6cb9e6
t-i-0414 <54778335+t-i-0414@users.noreply.github.com>
2019-12-09 02:44:47 +0900

    Development (#11)
    * application.html.erbを一時無効化、静的ビュー群にtitleタグをそれぞれ直接タグを追加。minitest,rspec（GREEN)
    
    * application.html.erbの利用開始。static_pages#rest_passwordをstatic_pages#reset_passwordに名称を修正

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,51 +3,55 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = 'The Trip Tip'
+  end
+
   test 'should get home' do
     get static_pages_home_url
     assert_response :success, @response.body
-    assert_select 'title', 'トップ | The Trip Tip'
+    assert_select 'title', "トップ | #{@base_title}"
   end
 
   test 'should get signup' do
     get static_pages_signup_url
     assert_response :success
-    assert_select 'title', 'ユーザー登録 | The Trip Tip'
+    assert_select 'title', "ユーザー登録 | #{@base_title}"
   end
 
   test 'should get login' do
     get static_pages_login_url
     assert_response :success
-    assert_select 'title', 'ログイン | The Trip Tip'
+    assert_select 'title', "ログイン | #{@base_title}"
   end
 
   test 'should get unlock_user' do
     get static_pages_unlock_user_url
     assert_response :success
-    assert_select 'title', 'アカウントのロック解除リクエスト | The Trip Tip'
+    assert_select 'title', "アカウントのロック解除リクエスト | #{@base_title}"
   end
 
-  test 'should get rest_password' do
-    get static_pages_rest_password_url
+  test 'should get reset_password' do
+    get static_pages_reset_password_url
     assert_response :success
-    assert_select 'title', 'パスワードのリセットリクエスト | The Trip Tip'
+    assert_select 'title', "パスワードのリセットリクエスト | #{@base_title}"
   end
 
   test 'should get about' do
     get static_pages_about_url
     assert_response :success
-    assert_select 'title', 'The Trip Tipについて | The Trip Tip'
+    assert_select 'title', "The Trip Tipについて | #{@base_title}"
   end
 
   test 'should get terms_of_use' do
     get static_pages_terms_of_use_url
     assert_response :success
-    assert_select 'title', '利用規約 | The Trip Tip'
+    assert_select 'title', "利用規約 | #{@base_title}"
   end
 
   test 'should get privacy' do
     get static_pages_privacy_url
     assert_response :success
-    assert_select 'title', 'プライバシーポリシー | The Trip Tip'
+    assert_select 'title', "プライバシーポリシー | #{@base_title}"
   end
 end
