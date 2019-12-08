8812b6b149863a10371b4d68edb7c99fa462ca1c
t-i-0414 <gutlo.ol0414@gmail.com>
2019-12-08 16:40:05 +0900

    Gemfileの編集(rspecの導入)*要bundle install。rspec関連ファイルの作成。テストのためにapplication.rbにホワイトリストとして'.www.example.com'を追加。static_pages_spec.rbの作成(green)。*要rails db:migrate

--- config/application.rb
+++ config/application.rb
@@ -15,10 +15,21 @@ module TheTripTip
     config.load_defaults 6.0
     config.hosts << '.amazonaws.com'
     config.hosts << '.paiza-user.cloud'
+    config.hosts << '.www.example.com'
     # Settings in config/environments/* take precedence over those specified
     # here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: true,
+                       request_specs: false
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+    end
   end
 end
