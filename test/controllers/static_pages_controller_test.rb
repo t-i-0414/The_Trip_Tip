89204f3f8feb4e44b700eea4b0fdb045a083c92d
t-i-0414 <gutlo.ol0414@gmail.com>
2019-12-09 03:15:01 +0900

    root_urlについてもminitest追加(RED)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,6 +7,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = 'The Trip Tip'
   end
 
+  test 'should get root' do
+    get root_url
+    assert_response :success, @response.body
+    assert_select 'title', "#{@base_title}"
+  end
+
   test 'should get home' do
     get static_pages_home_url
     assert_response :success, @response.body
@@ -21,37 +27,37 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test 'should get login' do
     get static_pages_login_url
-    assert_response :success
+    assert_response :success, @response.body
     assert_select 'title', "ログイン | #{@base_title}"
   end
 
   test 'should get unlock_user' do
     get static_pages_unlock_user_url
-    assert_response :success
+    assert_response :success, @response.body
     assert_select 'title', "アカウントのロック解除リクエスト | #{@base_title}"
   end
 
   test 'should get reset_password' do
     get static_pages_reset_password_url
-    assert_response :success
+    assert_response :success, @response.body
     assert_select 'title', "パスワードのリセットリクエスト | #{@base_title}"
   end
 
   test 'should get about' do
     get static_pages_about_url
-    assert_response :success
+    assert_response :success, @response.body
     assert_select 'title', "The Trip Tipについて | #{@base_title}"
   end
 
   test 'should get terms_of_use' do
     get static_pages_terms_of_use_url
-    assert_response :success
+    assert_response :success, @response.body
     assert_select 'title', "利用規約 | #{@base_title}"
   end
 
   test 'should get privacy' do
     get static_pages_privacy_url
-    assert_response :success
+    assert_response :success, @response.body
     assert_select 'title', "プライバシーポリシー | #{@base_title}"
   end
 end
