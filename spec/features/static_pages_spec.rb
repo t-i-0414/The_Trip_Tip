f4cf462dffda6aad59e1c8c0e4f0a90bf6631ee2
t-i-0414 <54778335+t-i-0414@users.noreply.github.com>
2020-01-15 11:55:21 +0900

    ルーティングの整理。static_pagesのviewをリファクタリング。テストの整理。 (#31)

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -9,34 +9,6 @@ describe 'Home' do
   end
 end
 
-describe 'signup' do
-  specify '画面の表示' do
-    visit '/signup'
-    expect(page).to have_title('ユーザー登録 | The Trip Tip')
-  end
-end
-
-describe 'login' do
-  specify '画面の表示' do
-    visit '/login'
-    expect(page).to have_title('ログイン | The Trip Tip')
-  end
-end
-
-describe 'unlock_user' do
-  specify '画面の表示' do
-    visit '/unlock_user'
-    expect(page).to have_title('アカウントのロック解除リクエスト | The Trip Tip')
-  end
-end
-
-describe 'rest_password' do
-  specify '画面の表示' do
-    visit '/reset_password'
-    expect(page).to have_title('パスワードのリセットリクエスト | The Trip Tip')
-  end
-end
-
 describe 'about' do
   specify '画面の表示' do
     visit '/about'
