a81a212106f26cb3205bb4f21f4733c65dda08c8
t-i-0414 <54778335+t-i-0414@users.noreply.github.com>
2020-01-15 17:16:34 +0900

    * 404と500のエラーページの実装完了。（レスポンシブ化はまだ）

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/terms_of_use', to: 'static_pages#terms_of_use'
   get '/privacy', to: 'static_pages#privacy'
+  
+    # 例外
+  get '*not_found', to: 'application#routing_error'
+  post '*not_found', to: 'application#routing_error'
 end
