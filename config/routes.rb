db0ca0a3688525d40cf415373b46fea9a0eb5a2d
Takuya Iwashiro <gutlo.ol0414@gmail.com>
2020-01-15 02:54:31 +0000

    ルーティングの整理。static_pagesのviewをリファクタリング。テストの整理。

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@
 
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get '/signup', to: 'static_pages#signup'
-  get '/login', to: 'static_pages#login'
-  get '/unlock_user', to: 'static_pages#unlock_user'
-  get '/reset_password', to: 'static_pages#reset_password'
   get '/about', to: 'static_pages#about'
   get '/terms_of_use', to: 'static_pages#terms_of_use'
   get '/privacy', to: 'static_pages#privacy'
