9716e74cb9a28fe3e3939650767a57d92ee590fd
t-i-0414 <gutlo.ol0414@gmail.com>
2020-01-17 00:01:57 +0900

    404と500エラーページの実装完了。（レスポンシブ済）。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,24 +2,24 @@
 
 # default class
 class ApplicationController < ActionController::Base
-  #protect_from_forgery with: :exception
-  
-  unless Rails.env.production?
+  # protect_from_forgery with: :exception
+
+  unless Rails.env.development?
     rescue_from Exception,                      with: :render_500
     rescue_from ActiveRecord::RecordNotFound,   with: :render_404
     rescue_from ActionController::RoutingError, with: :render_404
   end
- 
+
   def routing_error
     raise ActionController::RoutingError, params[:path]
   end
- 
+
   private
- 
+
   def render_404
     render 'error/404', status: :not_found
   end
- 
+
   def render_500
     render 'error/500', status: :internal_server_error
   end
