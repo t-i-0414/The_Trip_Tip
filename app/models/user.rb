5342ac44533434a9d54b95326062501315f4b78a
t-i-0414 <gutlo.ol0414@gmail.com>
2020-01-25 17:19:38 +0900

    SNS認証でのログイン機能の挙動確認。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,28 @@
-  # frozen_string_literal: true
+# frozen_string_literal: true
 
-  class User < ApplicationRecord
-    # Include default devise modules. Others available are:
-    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-    devise :database_authenticatable, :registerable,
-           :recoverable, :rememberable, :validatable,
-           :trackable, :confirmable, :lockable, :timeoutable,
-           :omniauthable
-    validates :name, presence: true
-
-    def self.from_omniauth(auth)
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-        user.provider = auth.provider
-        user.uid = auth.uid
-        user.name = auth.name
-        user.email = auth.info.email
-        user.password = Devise.friendly_token[0, 20] # ランダムなパスワードを作成
-        user.image = auth.info.image.gsub("_normal","") if user.provider == "twitter"
-        user.image = auth.info.image.gsub("picture","picture?type=large") if user.provider == "facebook"
-      end
-    end
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :trackable, :confirmable, :lockable, :timeoutable,
+         :omniauthable
+  with_options if: proc { |a| a.uid.blank? } do |a|
+    a.validates :name, presence: true
   end
+
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+    unless user
+      user = User.new(
+        provider: auth.provider,
+        uid: auth.uid,
+        name: auth.info.name,
+        email: auth.info.email,
+        password: Devise.friendly_token[6, 16]
+      )
+      user.image = auth.info.image.gsub('_normal', '') if user.provider == 'twitter'
+      user.image = auth.info.image.gsub('picture', 'picture?type=large') if user.provider == 'facebook'
+      user.save
+    end
+    user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/terms_of_use', to: 'static_pages#terms_of_use'
   get '/privacy', to: 'static_pages#privacy'
+
   # exception
   get '*not_found', to: 'application#routing_error'
   post '*not_found', to: 'application#routing_error'
