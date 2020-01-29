ddaf9ce012a5a9123aa85c89f4b30a6091af13d6
t-i-0414 <gutlo.ol0414@gmail.com>
2020-01-30 02:34:27 +0900

    的ページのコントローラーを修正。

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,15 +2,11 @@
 
 # default class
 class StaticPagesController < ApplicationController
-  # def home
-  # end
+  def home; end
 
-  # def about
-  # end
+  def about; end
 
-  # def terms_of_use
-  # end
+  def terms_of_use; end
 
-  # def privacy
-  # end
+  def privacy; end
 end
