c23601bf4e008a38df145ff830a889af35132d01
t-i-0414 <54778335+t-i-0414@users.noreply.github.com>
2020-01-30 02:05:42 +0900

    gem rubocopの導入。eslintにvue導入。jqueryの導入。 (#41)

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,19 @@
-const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
+const {
+  environment
+} = require('@rails/webpacker');
+const {
+  VueLoaderPlugin
+} = require('vue-loader');
+const vue = require('./loaders/vue');
+const webpack = require('webpack');
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
-module.exports = environment
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin());
+environment.loaders.prepend('vue', vue);
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+);
+module.exports = environment;
