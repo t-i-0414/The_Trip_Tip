e5f6c09f275b0683c9f47f97c79cfc4fda293d19
t-i-0414 <gutlo.ol0414@gmail.com>
2020-01-30 22:01:19 +0900

    jQueryの削除。

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,14 +5,8 @@ const {
   VueLoaderPlugin
 } = require('vue-loader');
 const vue = require('./loaders/vue');
-const webpack = require('webpack');
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin());
 environment.loaders.prepend('vue', vue);
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
-  })
-);
+
 module.exports = environment;
