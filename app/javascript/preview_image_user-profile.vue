b630bb8f14caa0a4c292330c507444381a6529bb
t-i-0414 <gutlo.ol0414@gmail.com>
2020-01-31 01:18:05 +0900

    プロフィール画像選択時のプレビュー機能に関する、jsとvueファイルを作成。

---
+++ app/javascript/preview_image_user-profile.vue
@@ -0,0 +1,12 @@
+<template> </template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      // message: 'Hello Vue!' 選択画像のプレビュー機能を実装予定。
+    };
+  }
+};
+console.log('test');
+</script>
