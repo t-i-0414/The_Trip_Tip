84f6b7820ffd43dd8bd3c86cbf97579d6eb1e9eb
t-i-0414 <gutlo.ol0414@gmail.com>
2020-02-02 03:40:05 +0900

    背景の回転vue実装。

---
+++ app/javascript/packs/rotate_background.js
@@ -0,0 +1,18 @@
+import Vue from 'vue/dist/vue.esm';
+
+$(() => {
+  new Vue({
+    el: '#background-rotate',
+    mounted() {
+      this.$nextTick(function () {
+        let count = 393;
+        setInterval(() => {
+          $(".bg").css(
+            "background", "linear-gradient(" + (count % 360) + "deg, #8fd3f4 0%, #8ed5ef 24%, #8cdce3 48%, #88e8ce 73%, #84f8b1 99%, #84fab0 100%)"
+          );
+          count++;
+        }, 30);
+      });
+    }
+  });
+});
