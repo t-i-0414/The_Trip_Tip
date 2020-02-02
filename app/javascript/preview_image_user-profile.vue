21f7a5798a1e8f66dd0423670dc04df047441e20
t-i-0414 <gutlo.ol0414@gmail.com>
2020-02-03 01:29:31 +0900

    javascript関係の整理。ユーザー画像のプレビュー表示まで完了。

--- app/javascript/preview_image_user-profile.vue
+++ app/javascript/preview_image_user-profile.vue
@@ -1,12 +1,39 @@
-<template> </template>
+<template>
+  <div class="field" id="preview_user_image">
+    <label for="user_image">プロフィール画像（任意）</label>
+    <label class="fileup-btn" for="user_image" :style="{ backgroundImage: 'url(' + data.image + ')' }"></label>
+    <input
+      class="hidden"
+      type="file"
+      name="user[image]"
+      id="user_image"
+      accept="image/*"
+      ref="file"
+      @change="setImage"
+    />
+  </div>
+</template>
 
 <script>
 export default {
-  data: function() {
+  data() {
     return {
-      // message: 'Hello Vue!' 選択画像のプレビュー機能を実装予定。
+      data: {
+        image: '/assets/icon_plus.svg',
+        name: ''
+      }
     };
+  },
+  methods: {
+    setImage(e) {
+      const files = this.$refs.file;
+      const fileImg = files.files[0];
+      if (fileImg.type.startsWith('image/')) {
+        this.data.image = window.URL.createObjectURL(fileImg);
+        this.data.name = fileImg.name;
+        this.data.type = fileImg.type;
+      }
+    }
   }
 };
-console.log('test');
 </script>
