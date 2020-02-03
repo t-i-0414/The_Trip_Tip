dde40abfce2227538002831c67284f2a369b2097
Takuya Iwashiro <gutlo.ol0414@gmail.com>
2020-02-03 04:22:34 +0000

    jsファイルの整理。

--- app/javascript/location_hash.js
+++ app/javascript/location_hash.js
@@ -1,4 +1,4 @@
-(() => {
+$(() => {
     if (window.location.hash && window.location.hash == '#_=_') {
     if (window.history && history.pushState) {
       window.history.pushState("", document.title, window.location.pathname);
