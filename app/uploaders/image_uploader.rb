7b2848a29fd1c85d3fd3f148b03bbace6e4097eb
t-i-0414 <54778335+t-i-0414@users.noreply.github.com>
2020-02-09 03:56:06 +0900

    image_uploaderの修正。 (#55)
    * ユーザー登録時のプロフィール画像機能の実装完了。
    
    * image_uploaderの修正。

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -17,7 +17,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url(*args)
+  def default_url(*)
     #   # For Rails 3.1+ asset pipeline compatibility:
     #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
     '/images/' + [version_name, 'default.png'].compact.join('_')
