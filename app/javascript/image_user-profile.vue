8810414582610b61672d823447bb3d810f64c05b
t-i-0414 <54778335+t-i-0414@users.noreply.github.com>
2020-02-08 14:18:08 +0900

    ユーザープロフィール画像のアップロード機能を実装。SNSリンクでの登録時のアイコンをbase64に要変換。 (#53)
    * axiosにしようか検討中。
    
    * axiosの実装途中。
    
    * ユーザープロフィール画像のアップロード機能を実装。SNSリンクでの登録時のアイコンをbase64に要変換。

--- app/javascript/image_user-profile.vue
+++ app/javascript/image_user-profile.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="preview_user_image">
     <div class="field">
-      <label for="user_image-before"> プロフィール画像（任意）</label>
-      <label class="fileup-btn" for="user_image-before" :style="{ backgroundImage: 'url(' + cropImg + ')' }"></label>
+      <label for="user_image-before">プロフィール画像（任意）</label>
+      <label
+        class="fileup-btn"
+        for="user_image-before"
+        :style="{ backgroundImage: 'url(' + cropImg + ')' }"
+      ></label>
       <input
         class="hidden"
         id="user_image-before"
         ref="input"
         type="file"
-        name=""
+        name
         accept="image/*"
         @change="setImage"
       />
-
       <div v-if="imgSrc != ''" :key="imgSrc" class="view-trimming">
         <vue-cropper
           ref="cropper"
@@ -28,34 +31,21 @@
           :img-style="{ width: '200px', height: '200px' }"
           :aspect-ratio="yoko / tate"
         ></vue-cropper>
-
-        <div class="trimming" @click="cropImage" v-if="imgSrc != ''">
-          画像を切り抜く
-        </div>
-        <div class="trimming" @click="removeImage" v-if="imgSrc != ''">
-          画像選択を解除する
-        </div>
-        <a class="downloader" v-if="cropImg != '/assets/icon_plus.svg'" :href="cropImg" :download="filename">
-          切り抜いた画像を保存する（任意）
-        </a>
+        <div class="trimming" @click="cropImage" v-if="imgSrc != ''">画像を切り抜く</div>
+        <div class="trimming" @click="removeImage" v-if="imgSrc != ''">画像選択を解除する</div>
+        <a
+          class="downloader"
+          v-if="cropImg != '/assets/icon_plus.svg'"
+          :href="cropImg"
+          :download="filename"
+        >切り抜いた画像を保存する（任意）</a>
+        <input class="hidden" id="user_base64" name="user[base64]" type="text" :value="userBase64" />
       </div>
-    </div>
-
-    <div class=" actions">
-      <input
-        @click="upload"
-        type="submit"
-        name="commit"
-        value="ユーザー登録"
-        class="btn"
-        data-disable-with="ユーザー登録"
-      />
     </div>
   </div>
 </template>
-
 <script>
-// import axios from 'axios';
+import Vue from 'vue/dist/vue.esm';
 import VueCropper from 'vue-cropperjs';
 import 'cropperjs/dist/cropper.css';
 
@@ -69,6 +59,7 @@ export default {
       tate: 1,
       imgSrc: '',
       cropImg: '/assets/icon_plus.svg',
+      userBase64: '',
       filename: '',
       resizedBlob: ''
     };
@@ -76,7 +67,7 @@ export default {
   methods: {
     setImage(e) {
       let count = e.target.files.length - 1;
-      let file = e.target.files[count];
+      const file = e.target.files[count];
       this.filename = file.name;
       if (!file.type.includes('image/')) {
         alert('画像ファイルを選択してください。');
@@ -92,64 +83,19 @@ export default {
         alert('この画像ファイルには対応しておりません。');
       }
     },
+
+    cropImage() {
+      this.cropImg = this.$refs.cropper.getCroppedCanvas().toDataURL();
+      this.userBase64 = this.$refs.cropper.getCroppedCanvas().toDataURL();
+      this.resizedBlob = this.base64ToBlob(this.cropImg);
+    },
+
     removeImage() {
       this.imgSrc = '';
       this.cropImg = '/assets/icon_plus.svg';
+      this.userBase64 = '';
       this.filename = '';
-    },
-    cropImage() {
-      this.cropImg = this.$refs.cropper.getCroppedCanvas().toDataURL();
-      this.resizedBlob = this.base64ToBlob(this.cropImg);
-      const resizedImg = window.URL.createObjectURL(this.resizedBlob);
-      const form = $('#new_user')[0];
-
-      let formData = new FormData(form);
-      // let request = new XMLHttpRequest();
-      // request.open('POST', '/users', false);
-      formData.append('user[image]', this.resizedBlob, 'image.png');
-
-      // $('#new_user').addEventListener('formdata', eve => {
-
-      // request.send(formData);
-
-      // form.append('user[image]', resizedBlob);
-
-      // console.log(form.get('user[image]'));
-      // let config = {
-      //   headers: {
-      //     'content-type': 'multipart/form-data'
-      //   }
-      // };
-      // axios
-      //   .post('/users', formData, config)
-      //   .then(function(response) {
-      //     console.log(response);
-      //   })
-      //   .catch(function(error) {
-      //     console.log(error);
-      //   });
-    },
-
-    upload(event) {
-      const form = $('#new_user')[0];
-      const formData = new FormData(form);
-      const request = new XMLHttpRequest();
-      request.open('POST', '/users');
-      formData.append('user[image]', this.resizedBlob, 'image.png');
-      request.send(formData);
-      // let config = {
-      //   headers: {
-      //     'content-type': 'multipart/form-data'
-      //   }
-      // };
-      // axios
-      //   .post('yourUploadUrl', formData, config)
-      //   .then(function(response) {
-      //     // response 処理
-      //   })
-      //   .catch(function(error) {
-      //     // error 処理
-      //   });
+      this.resizedBlob = '';
     },
 
     base64ToBlob(base64) {
@@ -164,6 +110,9 @@ export default {
     }
   }
 };
+$(document).ready(function() {
+  $('#user_base64').val(null);
+});
 </script>
 
 <style scoped>
