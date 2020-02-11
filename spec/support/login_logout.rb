9f842e27256efafc2f897568fdc3d2c452e1e514
t-i-0414 <54778335+t-i-0414@users.noreply.github.com>
2020-02-11 19:13:53 +0900

    静的ページ等についてのテスト実装。 (#56)

---
+++ spec/support/login_logout.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module LoginMacros
+  def login(user)
+    visit sign_in_path
+    fill_in 'email', with: user.email
+    fill_in 'password', with: '12345678'
+    click_button 'ログイン'
+    expect(page).to have_content 'ログインしました。'
+  end
+
+  def logout
+    click_link 'ログアウト'
+    expect(page).to have_content 'ログアウトしました。'
+  end
+
+  def act_as(user)
+    login user
+    yield
+    logout
+  end
+end
