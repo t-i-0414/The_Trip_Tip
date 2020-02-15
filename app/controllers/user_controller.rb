01a80b9052c0c6ffe41295f2c1ca51c9c342f938
t-i-0414 <gutlo.ol0414@gmail.com>
2020-02-15 19:52:18 +0900

    ユーザー詳細ページの実装。

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,5 +3,7 @@
 class UserController < ApplicationController
   def show
     @user = User.find(params[:id])
+    # @posts = Post.all.order(created_at: :desc)
+    # @posts = Post.page(params[:page]).per(20)
   end
 end
