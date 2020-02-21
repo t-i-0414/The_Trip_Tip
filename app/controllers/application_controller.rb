c387bd329e0c349c44916bb88b8bca1d2d770bc0
t-i-0414 <gutlo.ol0414@gmail.com>
2020-02-22 01:55:19 +0900

    ユーザー登録後、ログイン後のリダイレクト先を変更。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,4 +31,21 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name image])
   end
+
+  def after_update_path_for(_resource)
+    user_path(id: current_user.id)
+  end
+
+  # The path used after sign up for inactive accounts.
+  def after_inactive_sign_up_path_for(_resource)
+    user_path(id: current_user.id)
+  end
+
+  def after_sign_in_path_for(_resource)
+    user_path(id: current_user.id)
+  end
+
+  def after_sign_out_path_for(_resource)
+    root_path
+  end
 end
