fe9e863910d457de5dd3fd72c2f4005662ebc0a5
t-i-0414 <gutlo.ol0414@gmail.com>
2020-02-25 03:48:12 +0900

    user_sessionテスト実装中。

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -34,19 +34,46 @@ RSpec.feature 'Feature Static Pages', type: :feature do
       visit root_path
     end
 
-    scenario 'Header' do
-      expect(page).to have_link 'The Trip Tipのロゴ', href: root_path
-      expect(page).to have_link 'ユーザー登録', href: new_user_registration_path, count: 2
-      expect(page).to have_link 'ログイン', href: new_user_session_path, count: 2
+    scenario 'Header(bofore login)' do
+      expect(page.find('header')).to have_link 'The Trip Tipのロゴ', href: root_path
+
       # 以下はまだ未実装
       # 投稿一覧のリンク
-      # ログイン後のリンク集
+      expect(page.find('header')).to have_link 'ユーザー登録', href: new_user_registration_path, count: 2
+      expect(page.find('header')).to have_link 'ログイン', href: new_user_session_path, count: 2
+
+      expect(page.find('input.unshown#nav-input')[:type]).to eq 'checkbox'
+      expect(page.find('label.nav-close#nav-close')[:for]).to eq 'nav-input'
+      expect(page.find('label.nav-open#nav-open')[:for]).to eq 'nav-input'
+    end
+
+    scenario 'Header(after login)' do
+      visit new_user_session_path
+      users = FactoryBot.build_list(:user, 10, created_at: Time.current, updated_at: Time.current, confirmed_at: Time.current)
+      User.insert_all users.map(&:attributes)
+      @user = User.find_by(id: 1)
+      login(@user)
+
+      expect(page.find('label.user_detail_open#userDetailOpen')[:for]).to eq 'userDetailInput'
+      expect(page.find('#userDetailOpen').find('img')[:src]).to eq @user.image.url
+
+      expect(page.find('header')).to have_link 'The Trip Tipのロゴ', href: '/user/1'
+
+      # 以下はまだ未実装
+      # 投稿一覧のリンク
+      # ユーザー一覧のリンク
+      # 設定のリンク
+      expect(page.find('header')).to have_link 'ログアウト', href: destroy_user_session_path, count: 2
+
+      expect(page.find('input.unshown#nav-input')[:type]).to eq 'checkbox'
+      expect(page.find('label.nav-close#nav-close')[:for]).to eq 'nav-input'
+      expect(page.find('label.nav-open#nav-open')[:for]).to eq 'nav-input'
     end
 
     scenario 'Footer' do
-      expect(page).to have_link 'The Trip Tipについて', href: about_path
-      expect(page).to have_link '利用規約', href: terms_of_use_path
-      expect(page).to have_link 'プライバシーポリシー', href: privacy_path
+      expect(page.find('footer')).to have_link 'The Trip Tipについて', href: about_path
+      expect(page.find('footer')).to have_link '利用規約', href: terms_of_use_path
+      expect(page.find('footer')).to have_link 'プライバシーポリシー', href: privacy_path
     end
   end
 
@@ -55,6 +82,9 @@ RSpec.feature 'Feature Static Pages', type: :feature do
       visit '/tasks/404test'
       expect(page).to have_title full_title('404 Not Found')
       expect(page).to have_http_status 404
+      expect(page.find('h1')).to have_content '404 Not Found'
+      expect(page).to have_link '前のページに戻る', href: 'javascript:history.back()'
+      expect(page).to have_link 'トップページへ', href: root_path
     end
 
     scenario '500' do
@@ -62,6 +92,9 @@ RSpec.feature 'Feature Static Pages', type: :feature do
       visit about_path
       expect(page).to have_title full_title('500 Internal Server Error')
       expect(page).to have_http_status 500
+      expect(page.find('h1')).to have_content '500 Internal Server Error'
+      expect(page).to have_link '前のページに戻る', href: 'javascript:history.back()'
+      expect(page).to have_link 'トップページへ', href: root_path
     end
   end
 end
