c5090795832a0bcf0fc41af2c1802717cf6d796c
t-i-0414 <gutlo.ol0414@gmail.com>
2020-02-29 06:27:11 +0900

    認証メールのテストまで完了。

--- spec/support/confirmation_email.rb
+++ spec/support/confirmation_email.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
-def confirmation_email(user)
-
+def confirmation_email
   mail = ActionMailer::Base.deliveries.last
+  mail_body = mail.body.encoded
 
-  def extract_confirmation_url(mail)
-    body = mail.body.encoded
-    # body[/http[^"]/+]
-  end
+  confirmation_link = mail_body.match(%r{http[^"]+users\/confirmation[^"]+})
+
+  visit confirmation_link
 end
