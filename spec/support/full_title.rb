e40efc7fe505d49619532757b15d08c259e1243b
Takuya Iwashiro <gutlo.ol0414@gmail.com>
2020-02-28 08:33:58 +0000

    ファイル名修正。

--- spec/support/full_title.rb
+++ spec/support/full_title.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
-def confirmation_email(user)
-
-  mail = ActionMailer::Base.deliveries.last
-
-  def extract_confirmation_url(mail)
-    body = mail.body.encoded
-    # body[/http[^"]/+]
+  def full_title(page_title)
+    base_title = 'The Trip Tip'
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
   end
-end
