45f05eaf9446ccd0850724d4c825b748453d7b3d
t-i-0414 <gutlo.ol0414@gmail.com>
2020-02-29 04:47:15 +0900

    テストのリファクタリング。それに伴う修正。

--- spec/features/users_session_spec.rb
+++ spec/features/users_session_spec.rb
@@ -4,13 +4,8 @@ require 'rails_helper'
 
 RSpec.feature 'Feature Users Sessions', type: :feature do
   before(:each) do
-    users = FactoryBot.create_list(:user, 10, created_at: Time.current, updated_at: Time.current, confirmed_at: Time.current)
-    @user = users[0]
+    init_db_test
     visit new_user_session_path
-  end
-  
-  before do
-    ActionMailer::Base.deliveries.clear
   end
 
   # def extract_confirmation_url(mail)
@@ -20,58 +15,17 @@ RSpec.feature 'Feature Users Sessions', type: :feature do
 
   feature 'Render right contents' do
     scenario 'Before login' do
-      twitter = page.find('.sns-link.btn.twitter', text: 'twitterでログイン')
-      facebook = page.find('.sns-link.btn.facebook', text: 'Facebookでログイン')
-      email_field = page.all('.field')[0]
-      email_label = email_field.find('.form-label')
-      email_input = email_field.find('#user_email')
-
-      password_field = page.all('.field')[1]
-      password_label = password_field.find('.form-label')
-      password_input = password_field.find('#user_password')
-
       remember_me_field = page.all('.field')[2]
-      remember_me_label = remember_me_field.find('label')
-      remember_me_input_check = remember_me_field.find('#user_remember_me')
-      remember_me_input_hidden = remember_me_field.all('input', visible: false)[0]
-
-      submit_field = page.find('.actions.login')
-      submit_input = submit_field.find('input')
-
       notification_field = page.find('.wrapper_notification')
 
       expect(page).to have_http_status 200
       expect(page).to have_title full_title('ユーザーログイン')
       expect(page).to have_selector 'h1', text: 'ログイン'
 
-      expect(twitter[:href]).to eq user_twitter_omniauth_authorize_path
-      expect(facebook[:href]).to eq user_facebook_omniauth_authorize_path
-
-      expect(email_field).to have_field('メールアドレス')
-      expect(email_label[:for]).to eq 'user_email'
-      expect(email_input[:autofocus]).to eq 'autofocus'
-      expect(email_input[:autocomplete]).to eq 'email'
-      expect(email_input[:name]).to eq 'user[email]'
-
-      expect(password_field).to have_field('パスワード')
-      expect(password_label[:for]).to eq 'user_password'
-      expect(password_input[:autocomplete]).to eq 'current-password'
-      expect(password_input[:placeholder]).to eq '半角英数字6文字以上16文字以内'
-      expect(password_input[:type]).to eq 'password'
-      expect(password_input[:name]).to eq 'user[password]'
+      expect(page).to have_link 'twitterでログイン', href:user_twitter_omniauth_authorize_path
+      expect(page).to have_link 'Facebookでログイン', href:user_facebook_omniauth_authorize_path
 
       expect(remember_me_field).to have_field('次回から自動的にログイン')
-      expect(remember_me_label[:for]).to eq 'user_remember_me'
-      expect(remember_me_input_hidden[:name]).to eq 'user[remember_me]'
-      expect(remember_me_input_hidden[:type]).to eq 'hidden'
-      expect(remember_me_input_hidden[:value]).to eq '0'
-      expect(remember_me_input_check[:type]).to eq 'checkbox'
-      expect(remember_me_input_check[:value]).to eq '1'
-      expect(remember_me_input_check[:name]).to eq 'user[remember_me]'
-
-      expect(submit_input[:type]).to eq 'submit'
-      expect(submit_input[:name]).to eq 'commit'
-      expect(submit_input[:value]).to eq 'ログイン'
 
       expect(notification_field).to have_link 'パスワードをお忘れですか？', href: new_user_password_path
       expect(notification_field).to have_link 'ロックされたアカウントの解除', href: new_user_unlock_path
@@ -119,7 +73,7 @@ RSpec.feature 'Feature Users Sessions', type: :feature do
   feature 'New session' do
     scenario 'Success' do
       login(@user)
-      expect(page).to have_content 'ログインしました。'
+      expect(page).to have_title full_title("#{@user.name}")
     end
 
     scenario 'Fail' do
@@ -128,6 +82,7 @@ RSpec.feature 'Feature Users Sessions', type: :feature do
       click_button 'ログイン'
       expect(page).to have_title full_title('ユーザーログイン')
       expect(page).to have_content 'メールアドレスまたはパスワードが違います。'
+
       fill_in 'user[email]', with: @user.email
       fill_in 'user[password]', with: 'passward'
       click_button 'ログイン'
@@ -140,12 +95,12 @@ RSpec.feature 'Feature Users Sessions', type: :feature do
     scenario 'Do not redirect to root' do
       login(@user)
       visit '/'
-      expect(current_path).to eq user_path id: 1
+      expect(current_path).to eq user_path id: @user.id
     end
 
     scenario 'Do not post as other user' do
       login(@user)
-      visit user_path id: 2
+      visit user_path id: @user.id += 1
       user_drawer = page.find('.user_drawer')
       expect(user_drawer).to have_no_content 'つぶやきを投稿する'
       expect(user_drawer).to have_content 'フォローする'
