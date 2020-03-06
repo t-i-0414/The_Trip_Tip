4cad42f7836b5275f592d741ef930e43a78f0dd0
t-i-0414 <gutlo.ol0414@gmail.com>
2020-03-06 16:57:15 +0900

    ユーザー一覧のテスト実装。

--- spec/features/04user_index_spec.rb
+++ spec/features/04user_index_spec.rb
@@ -3,28 +3,34 @@
 require 'rails_helper'
 
 RSpec.feature 'Feature Users Sessions', type: :feature do
-  before(:each) do
+  before do
     init_db_test
   end
 
   feature 'Render right contents' do
     scenario 'Before login' do
       visit user_index_path
-      
+
       expect(page).to have_http_status 200
       expect(page).to have_title full_title('ユーザーログイン')
       expect(page).to have_selector 'h1', text: 'ログイン'
       expect(page).to have_content 'アカウント登録もしくはログインしてください。'
     end
-    
+
     scenario 'After login' do
       visit new_user_session_path
       login(@user)
-      
+
       visit user_index_path
       expect(page).to have_http_status 200
       expect(page).to have_title full_title('ユーザー一覧')
-      
+      expect(page).to have_css 'div.wrapper_pagenate.top'
+      expect(page).to have_css 'div.wrapper_pagenate.bottom'
+
+      @users.each do |user|
+        expect(page).to have_link 'ユーザーのアイコン', href: user_path(id: user.id)
+        expect(page).to have_link user.name, href: user_path(id: user.id)
+      end
     end
   end
 end
