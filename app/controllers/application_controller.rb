dbb0055e4f4e138cf5e8a38b0b05e3fbe3f425c2
t-i-0414 <gutlo.ol0414@gmail.com>
2020-03-09 05:10:40 +0900

    エラーページのコントローラー修正。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  unless Rails.env.production?
+  unless Rails.env.development?
     rescue_from Exception,                      with: :render_500
     rescue_from ActiveRecord::RecordNotFound,   with: :render_404
     rescue_from ActionController::RoutingError, with: :render_404
