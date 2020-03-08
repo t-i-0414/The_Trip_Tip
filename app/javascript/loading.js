dcd24495615067ed34f265c3d13c364b89f63e30
t-i-0414 <gutlo.ol0414@gmail.com>
2020-03-09 02:30:23 +0900

    ローディングの修正（IOS対策）

--- app/javascript/loading.js
+++ app/javascript/loading.js
@@ -2,16 +2,22 @@ $(window).on('load', () => {
   $('#loading').delay(100).fadeOut();
 });
 
-window.onbeforeunload = function () {
-  // IE用。
-};
-window.onunload = function () {
-  // IE以外用。
-};
+// window.onbeforeunload = function () {
+//   // IE用。
+// };
+// window.onunload = function () {
+//   // IE以外用。
+// };
 
-window.addEventListener("pageshow", function (event) {
+// window.addEventListener("pageshow", function (event) {
+//   if (event.persisted) {
+//     $('#loading').delay(100).fadeOut();
+//     window.location.reload();
+//   }
+// });
+
+window.onpageshow = function (event) {
   if (event.persisted) {
-    $('#loading').delay(100).fadeOut();
     window.location.reload();
   }
-});
+};
