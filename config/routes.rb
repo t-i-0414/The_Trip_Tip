571dff6358a0b9182e3eedc07a019b0e2a1b9d09
t-i-0414 <gutlo.ol0414@gmail.com>
2020-03-30 04:30:00 +0900

    テストユーザーでログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
+
   resources :user, only: %i[index show]
 
   authenticated :user do
