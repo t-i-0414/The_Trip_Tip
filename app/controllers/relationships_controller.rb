f7f85d4a1874f2f00c54aaf967380cd0406d5ee8
t-i-0414 <gutlo.ol0414@gmail.com>
2020-03-30 02:59:04 +0900

    いいね機能の実装

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -5,15 +5,13 @@ class RelationshipsController < ApplicationController
   # before_action :correct_user,   only: :destroy
 
   def create
-    user = User.find(params[:followed_id])
-    current_user.follow(user)
-    redirect_to user
+    @user = User.find(params[:followed_id])
+    current_user.follow(@user)
   end
 
   def destroy
-    user = Relationship.find(params[:id]).followed
-    current_user.unfollow(user)
-    redirect_to user
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user)
   end
 
   protected
