339713f9bf9e3ce8d801ae0627a482be0a7bd3f9
t-i-0414 <gutlo.ol0414@gmail.com>
2020-03-31 03:38:05 +0900

    いいね機能のテストの実装とそれに伴う既存のテストの修正

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,4 +3,6 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :micropost
+  validates :micropost_id, presence: true
+  validates :user_id, presence: true
 end
--- spec/models/04like_spec.rb
+++ spec/models/04like_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'Model Like', type: :model do
+  before :each do
+    init_db_test
+    @like = Like.create(micropost_id: 1, user_id: 1)
+  end
+
+  describe 'Validated Like' do
+    it "Valid" do
+      expect(@like).to be_valid
+    end
+
+    it "Require micropost_id" do
+      @like.micropost_id = nil
+      expect(@like).not_to be_valid
+    end
+
+    it "Require user_id" do
+      @like.user_id = nil
+      expect(@like).not_to be_valid
+    end
+  end
 end
