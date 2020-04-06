b7e6269e2d054f64f46ebea2049faedca6c12f02
t-i-0414 <gutlo.ol0414@gmail.com>
2020-04-07 02:10:24 +0900

    Capistrano実装に伴う微修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,10 +26,13 @@ set :deploy_to, '/var/www/rails/The_Trip_Tip'
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml"
 set :linked_files, fetch(:linked_files, []).push('config/settings.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+set :linked_files, fetch(:linked_files, []).push('config/master.key')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
