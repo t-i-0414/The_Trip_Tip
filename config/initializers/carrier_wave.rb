e87cdef6076030af8a5e68621e437959400f88f7
t-i-0414 <gutlo.ol0414@gmail.com>
2020-04-12 05:13:47 +0900

    fog-awsへの変更に伴う関連ファイルの変更

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,7 +4,7 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       # Amazon S3
-      provider: 'AWS',
+      provider: 'fog/aws',
       region: 'ap-northeast-1',
       aws_access_key_id: Rails.application.credentials.s3[:access_key_id],
       aws_secret_access_key: Rails.application.credentials.s3[:secret_access_key]
